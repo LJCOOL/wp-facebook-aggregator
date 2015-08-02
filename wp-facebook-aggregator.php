8b2a4539ec6b01ee31b2b6ba037112e0c344a6d2
LJCOOL <jay@Jays-MBP.gateway>
2015-08-02 11:14:55 +1000

    added plugin info

--- wp-facebook-aggregator.php
+++ wp-facebook-aggregator.php
@@ -1,3 +1,10 @@
 <?php
+/*
+Plugin Name: Wordpress Facebook Aggregator
+Plugin URI: http://github.com/LJCOOL/wp-facebook-aggregator
+Description: Pulls and displays posts from multiple Facebook pages.
+Version: 1.0
+Author: Jay Newton, Shaawin Vsingam
+*/
 
 ?>
