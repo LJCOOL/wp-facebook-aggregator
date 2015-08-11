0e40c166770f8d246ea2870a4322001238288dc1
Shaawin Vsingam <shaawin95@gmail.com>
2015-08-11 23:13:00 +1000

    added menu creation, placeholder options register, TODO generate_page

--- options.php
+++ options.php
@@ -1,6 +1,27 @@
 <?php
-
+//create custom plugin settings menu
 add_action('admin_menu', 'feed_options');
 
+function feed_options(){
+    //creates a top level menu
+    add_menu_page('WP Feed Aggregator Options',
+                  'Feed Aggregator Options',
+                  'manage_options',
+                  'wpfa-options',
+                  'generate_page'
+                );
+    //register settings
+    add_action('admin_init','register_options');
+}
+
+//defines attributes to be saved
+function register_options(){
+    //register settings to be saved
+    register_setting('settings-group','option-name');
+}
+
+function generate_page(){
+    //put html here to generate page
+}
 
  ?>
