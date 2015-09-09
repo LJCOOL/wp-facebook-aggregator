80629bd7c3980acc9ea354fccf8c830841ba6a3c
Shaawin Vsingam <shaawin95@gmail.com>
2015-09-09 20:09:05 +1000

    Add settings for images, links etc and beautify options page

--- options.php
+++ options.php
@@ -26,26 +26,32 @@ add_action( 'admin_enqueue_scripts', 'wpfa_scripts' );
 //defines attributes to be saved
 function register_options() {
     //sane quota for IDs
-    register_setting('id-group','page-ID1');
-    register_setting('id-group','page-ID2');
-    register_setting('id-group','page-ID3');
-    register_setting('id-group','page-ID4');
-    register_setting('id-group','page-ID5');
-    register_setting('id-group','page-ID6');
-    register_setting('id-group','page-ID7');
-    register_setting('id-group','page-ID8');
-    register_setting('id-group','page-ID9');
-    register_setting('id-group','page-ID10');
-    register_setting('id-group','page-ID11');
-    register_setting('id-group','page-ID12');
-    register_setting('id-group','page-ID13');
-    register_setting('id-group','page-ID14');
-    register_setting('id-group','page-ID15');
-    register_setting('id-group','page-ID16');
-    register_setting('id-group','page-ID17');
-    register_setting('id-group','page-ID18');
-    register_setting('id-group','page-ID19');
-    register_setting('id-group','page-ID20');
+    register_setting('wpfa-settings','page-ID1');
+    register_setting('wpfa-settings','page-ID2');
+    register_setting('wpfa-settings','page-ID3');
+    register_setting('wpfa-settings','page-ID4');
+    register_setting('wpfa-settings','page-ID5');
+    register_setting('wpfa-settings','page-ID6');
+    register_setting('wpfa-settings','page-ID7');
+    register_setting('wpfa-settings','page-ID8');
+    register_setting('wpfa-settings','page-ID9');
+    register_setting('wpfa-settings','page-ID10');
+    register_setting('wpfa-settings','page-ID11');
+    register_setting('wpfa-settings','page-ID12');
+    register_setting('wpfa-settings','page-ID13');
+    register_setting('wpfa-settings','page-ID14');
+    register_setting('wpfa-settings','page-ID15');
+    register_setting('wpfa-settings','page-ID16');
+    register_setting('wpfa-settings','page-ID17');
+    register_setting('wpfa-settings','page-ID18');
+    register_setting('wpfa-settings','page-ID19');
+    register_setting('wpfa-settings','page-ID20');
+
+    //settings for links, videos, images etc
+    register_setting('wpfa-settings','images');
+    register_setting('wpfa-settings','links');
+    register_setting('wpfa-settings','videos');
+    register_setting('wpfa-settings','redirect');
 }
 
 //add javascript file to be used by options page
@@ -57,9 +63,12 @@ function wpfa_scripts() {
 function generate_page() { ?>
     <div class="wrap">
     <h2>WP Feed Aggregator Options</h2>
+    <hr>
+    <h3 style="float:left">Facebook IDs</h3>
+    <p style="padding-top:4px;padding-left:10px;float:left"><em>(Leave blank to remove)</em></p>
     <form method="post" action="options.php">
-    <?php settings_fields( 'id-group' ); ?>
-    <?php do_settings_sections( 'id-group' ); ?>
+    <?php settings_fields( 'wpfa-settings' ); ?>
+    <?php do_settings_sections( 'wpfa-settings' ); ?>
     <table class="form-table">
         <!-- <div class="input_fields_wrap">
         <button class="add_field_button">Add More Fields</button>
@@ -68,7 +77,7 @@ function generate_page() { ?>
         <tr valign="top">
         <th scope="row">Facebook Page ID 1</th>
         <td><input type="text" name="page-ID1" value="<?php echo esc_attr( get_option('page-ID1') ); ?>" />
-        <i>eg. 123542974439976</i>  <a href="https://lookup-id.com/" target="_blank">Still don't know?</a></td>
+        <em> eg. 123542974439976</em>  <a href="https://lookup-id.com/" target="_blank">Still don't know?</a></td>
         </tr>
         <tr valign="top">
         <th scope="row">Facebook Page ID 2</th>
@@ -87,7 +96,26 @@ function generate_page() { ?>
         <td><input type="text" name="page-ID5" value="<?php echo esc_attr( get_option('page-ID5') ); ?>" /></td>
         </tr>
     </table>
-    </div>
+    <hr>
+    <h3>Other options</h3>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Show images</th>
+        <td><input type="checkbox" name="images" value="1" <?php checked( '1', get_option('images') ); ?> /></td>
+        </tr>
+        <tr valign="top">
+        <th scope="row">Get posts that are links</th>
+        <td><input type="checkbox" name="links" value="1" <?php checked( '1', get_option('links') ); ?> /></td>
+        </tr>
+        <tr valign="top">
+        <th scope="row">Get posts that have videos</th>
+        <td><input type="checkbox" name="videos" value="1" <?php checked( '1', get_option('videos') ); ?> /></td>
+        </tr>
+        <tr valign="top">
+        <th scope="row">Redirect featured link to category</th>
+        <td><input type="checkbox" name="redirect" value="1" <?php checked( '1', get_option('redirect') ); ?> /></td>
+        </tr>
+    </table>
     <?php submit_button(); ?>
     </form>
     </div>
