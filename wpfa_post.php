6d6e1d0fae1503b0db6c97a75ead8abdabe68b49
Shaawin Vsingam <shaawin95@gmail.com>
2015-10-03 20:49:19 +1000

    add function to get the resultant redirect of a URL.

--- wpfa_post.php
+++ wpfa_post.php
@@ -80,5 +80,18 @@ class wpfa_Post{
       }
       return $title[0];
     }
+
+    function getRedirectUrl($url) {
+        stream_context_set_default(array(
+            'http' => array(
+                'method' => 'HEAD'
+            )
+        ));
+        $headers = get_headers($url, 1);
+        if ($headers !== false && isset($headers['Location'])) {
+            return $headers['Location'];
+        }
+        return false;
+    }
 }
 ?>
